5064f0f842b403d7561c6d13c8f9ee8509463b07
John Doe <johndoe@example.com>
2023-03-29 22:35:57 +0900

    re-socket test polder upload

---
+++ socket_test_dir/socket_test_1/socket_test/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace socket_test
+{
+    partial class Start
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Server = new System.Windows.Forms.Button();
+            this.Client = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Server
+            // 
+            this.Server.Location = new System.Drawing.Point(113, 55);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(153, 45);
+            this.Server.TabIndex = 0;
+            this.Server.Text = "Server";
+            this.Server.UseVisualStyleBackColor = true;
+            this.Server.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Client
+            // 
+            this.Client.Location = new System.Drawing.Point(113, 157);
+            this.Client.Name = "Client";
+            this.Client.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Client.Size = new System.Drawing.Size(153, 43);
+            this.Client.TabIndex = 1;
+            this.Client.Text = "Client";
+            this.Client.UseVisualStyleBackColor = true;
+            this.Client.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.Client);
+            this.Controls.Add(this.Server);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.Load += new System.EventHandler(this.Start_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Server;
+        private System.Windows.Forms.Button Client;
+    }
+}
+
