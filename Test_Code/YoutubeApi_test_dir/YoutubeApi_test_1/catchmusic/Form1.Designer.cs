034adf5c4ad6030224154460db1fbf236a4cf268
garam0826 <garam0826@naver.com>
2023-05-01 17:46:37 +0900

    test1

---
+++ Test_Code/YoutubeApi_test_dir/YoutubeApi_test_1/catchmusic/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace catchmusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.txtSearchQuery = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(453, 31);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(83, 34);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "btnPlay";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // txtSearchQuery
+            // 
+            this.txtSearchQuery.Location = new System.Drawing.Point(146, 39);
+            this.txtSearchQuery.Name = "txtSearchQuery";
+            this.txtSearchQuery.Size = new System.Drawing.Size(279, 21);
+            this.txtSearchQuery.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "노래제목입력";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 364);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearchQuery);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtSearchQuery;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test_Code/YoutubeApi_test_dir/YoutubeApi_test_1/catchmusic/MusicPlayerMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catchmusic
+{
+    class MusicPlayerMain
+    {
+        private readonly YouTubeAPI youtubeAPI;
+
+        public MusicPlayerMain()
+        {
+            youtubeAPI = new YouTubeAPI();
+        }
+
+        public void PlayMusic(string searchQuery)
+        {
+            youtubeAPI.PlayMusic(searchQuery);
+        }
+    }
+}
