f1864381c9a680087a0f183f4df2c52a60d8d146
swjang7269 <azamiyuko7269@gmail.com>
2023-05-06 18:57:48 +0900

    Login test
    test1 - 로그인 화면 구현
    test2 - 로그인 성공여부, 회원가입 구현

---
+++ Test_Code/Login_test_dir/Login_test_1/Login_test_1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_test_1
+{
+    public partial class Form1 : Form
+    {
+        public static SQLite _db = new SQLite();
+
+        public bool test = false;
+        TextBox[] txtList;
+        const string IDPlaceholder = "아이디";
+        const string PWPlaceholder = "비밀번호";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //id, pw TextBox Placeholder 설정
+            txtList = new TextBox[] { txt_ID, txt_PW };
+            foreach (var txt in txtList)
+            {
+                txt.ForeColor = Color.DarkGray;
+                if (txt == txt_ID)
+                    txt.Text = IDPlaceholder;
+                else if (txt == txt_PW)
+                    txt.Text = PWPlaceholder;
+
+                txt.GotFocus += RmPlaceholder;
+                txt.LostFocus += SetPlaceholder;
+            }
+        }
+
+        private void RmPlaceholder(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if(txt.Text == IDPlaceholder | txt.Text == PWPlaceholder)
+            {
+                txt.ForeColor = Color.Black;
+                txt.Text = string.Empty;
+                if (txt == txt_PW) 
+                    txt_PW.PasswordChar = '●';
+            }
+        }
+
+        private void SetPlaceholder(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                txt.ForeColor = Color.DarkGray;
+                if (txt == txt_ID)
+                    txt.Text = IDPlaceholder;
+                else if (txt == txt_PW)
+                {
+                    txt.Text = PWPlaceholder;
+                    txt_PW.PasswordChar = default;
+                }
+            }
+        }
+
+        private void Signin_btn_Click(object sender, EventArgs e)
+        {
+            LoadUserInfo();
+            CheckID_PW(txt_ID.Text, txt_PW.Text);
+        }
+
+        public static void LoadUserInfo()
+        {
+            Config.user_ds = _db.SelectAll(Config.Tables[(int)eTName._user]);
+        }
+
+        private void CheckID_PW (string id, string pw)
+        {
+            if (Config.user_ds.Tables[0].Rows.Count > 0)
+            {
+                foreach(DataRow row in Config.user_ds.Tables[0].Rows)
+                {
+                    if (id == row["id"].ToString())
+                    {
+                        if (pw == row["pw"].ToString()) MessageBox.Show("로그인 성공");
+                        else MessageBox.Show("비밀번호가 일치하지 않습니다.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("사용자 정보가 없습니다.");
+            }
+        }
+    }
+}
---
+++ Test_Code/Login_test_dir/Login_test_1/Login_test_1/SQLite.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Login_test_1
+{
+    public class SQLite
+    {
+        SQLiteConnection conn = new SQLiteConnection($"server={Config.Server};uid={Config.UserID};" +
+            $"pwd={Config.UserPassword};database={Config.Database};pooling=false;allow user variables=true");
+
+        static string DBpath = Application.StartupPath + @"\database.db";
+        string dataSource = $@"Data Source={DBpath}";
+        SQLiteDataAdapter adpt;
+        SQLiteCommand cmd;
+        
+        public void Connection()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+            conn.Close();
+        }
+        public DataSet SelectAll(string table)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                string sql = $"SELECT * FROM {table}";
+                adpt = new SQLiteDataAdapter(sql, dataSource);
+                adpt.Fill(ds, table);
+
+                if (ds.Tables.Count > 0) return ds;
+                else return null;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public DataSet SelectDetail(string table, string condition, string where = "")
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                string sql = $"SELECT {condition} FROM {table} {where}";
+                adpt = new SQLiteDataAdapter(sql, dataSource);
+                adpt.Fill(ds, table);
+
+                if (ds.Tables.Count > 0) return ds;
+                else return null;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void Insert(string table, string value)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"INSERT INTO {table} VALUES ({value})";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void Update(string table, string setvalue, string wherevalue = "")
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"UPDATE {table} SET {setvalue} WHERE {wherevalue}";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void DeleteAll(string table)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"DELETE FROM {table}";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void DeleteDetail(string table, string wherecol, string wherevalue)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"DELETE FROM {table} WHERE {wherecol}='{wherevalue}'";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+    }
+}
