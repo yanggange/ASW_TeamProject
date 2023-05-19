9b6a06174774ffe0a65ae9650d74fbaae1ecfc9d
YangGyeongWon <didruddnjs55@naver.com>
2023-05-19 18:35:57 +0900

    fix datagridview

--- CatchMusic/Catch_Music/Catch_Music/Client.Designer.cs
+++ CatchMusic/Catch_Music/Catch_Music/Client.Designer.cs
@@ -118,6 +118,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(436, 77);
             this.dataGridView1.Name = "dataGridView1";
