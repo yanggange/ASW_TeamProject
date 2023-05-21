f1a26e1ded73b97fc705ffb7865f0e6310e5551d
garam0826 <garam0826@naver.com>
2023-05-22 00:10:07 +0900

    client 노래동시 재생

--- CatchMusic/Catch_Music/Catch_Music/Client.cs
+++ CatchMusic/Catch_Music/Catch_Music/Client.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -11,6 +12,11 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Catch_Music
 {
@@ -24,12 +30,133 @@ namespace Catch_Music
         public string musicTitle = null; // 방장이 음악 실행하면 음악의 제목이 저장될 변수
         public DataSet dataset = new ClientINFO();
 
+        private YouTubeService youtubeService;
+        private string videoId;
+        private string audioUrl;
+        private Process audioProcess;
+
         public Client()
         {
             InitializeComponent();
             dataGridView1.DataSource = dataset.Tables["clientINFO"];
+            youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCXYRYadXpJP9AzdPidWCYKVO_Xj5wcQM4", // Google API Console에서 생성한 인증키를 입력하세요.
+                ApplicationName = this.GetType().ToString()
+            });
         }
+        private string GetHtml(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string html = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+                return html;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("오류가 발생했습니다: " + ex.Message);
+                return null;
+            }
+        }
+        private string GetAudioUrl(string html)
+        {
+            int fintWord = html.IndexOf("adaptiveFormats") + 17; //+17
+            if (fintWord == -1)
+            {
+                return null;
+            }
 
+            int start = html.IndexOf("[", fintWord);
+            if (start == -1)
+            {
+                return null;
+            }
+
+            int end = html.IndexOf("]", start);
+            if (end == -1)
+            {
+                return null;
+            }
+
+            string json = html.Substring(start, end - start + 1);
+
+            if (!IsJson(json))
+            {
+                return null; // 만약 json 형식이 아니라면 null 반환
+            }
+
+            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            foreach (var item in data)
+            {
+                string mimeType = item.mimeType;
+                if (mimeType.StartsWith("audio/mp4"))
+                {
+                    string url = item.url;
+                    return url;
+                }
+            }
+
+            return null;
+        }
+        private bool IsJson(string input)
+        {
+            try
+            {
+                JToken.Parse(input);
+                return true;
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+        public void PlayMusic(string musicTitle)
+        {
+            string query = musicTitle;
+            SearchResource.ListRequest listRequest = youtubeService.Search.List("snippet");
+            listRequest.Q = query;
+            listRequest.MaxResults = 1;
+
+            SearchListResponse searchListResponse = listRequest.Execute();
+            if (searchListResponse.Items.Count == 0)
+            {
+                MessageBox.Show("검색 결과가 없습니다.");
+                return;
+            }
+
+            SearchResult result = searchListResponse.Items[0];
+            videoId = result.Id.VideoId;
+
+            string videoUrl = $"https://www.youtube.com/watch?v={videoId}";
+            string html = GetHtml(videoUrl);
+            if (html == null)
+            {
+                MessageBox.Show("비디오를 재생할 수 없습니다.");
+                return;
+            }
+
+            audioUrl = GetAudioUrl(html);
+            if (audioUrl == null)
+            {
+                MessageBox.Show("오디오를 재생할 수 없습니다.");
+                return;
+            }
+
+            string mpv_path = Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory)) + "\\MPV\\mpv.exe";
+
+            audioProcess = new Process();
+            audioProcess.StartInfo.FileName = mpv_path;
+            audioProcess.StartInfo.Arguments = $"--no-video {audioUrl}";
+            audioProcess.StartInfo.UseShellExecute = false;
+            audioProcess.StartInfo.RedirectStandardOutput = true;
+            audioProcess.StartInfo.CreateNoWindow = true;
+            audioProcess.Start();
+        }
         private void Client_Load(object sender, EventArgs e)
         {
 
@@ -184,8 +311,12 @@ namespace Catch_Music
                         ///
                         // 서버 폼에서 음악실행 버튼을 누르면
                         // 실행할 노래의 제목이 musicTitle에 저장됩니다.
-                        // 여기에 음악 실행하는 코드 작성(ce.musicTitle 변수가 음악 제목입니다)
-                        ///
+
+                        // 음악 실행하는 코드 작성(ce.musicTitle 변수가 음악 제목)
+                        //server에서 노래 재생 버튼을 누르면 시간차를 두고 client에서도 노래가 재생된다.
+                        //시연 할때는 아래 코드를 주석처리 할 예정(기능은 확인 됬으니 원활한 시연을 위해서)
+                        //ce.PlayMusic(ce.musicTitle);
+
                         continue;
                     }
 
